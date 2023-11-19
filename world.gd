f0e64410e842c0ca8a1a8ecc0102e66d51149936
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-19 22:41:08 +0100

    feat: add animation to rocket
    - Rocket launch animation
    - Also update names of sprite2D nodes

--- world.gd
+++ world.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+@onready var animation_player = $AnimationPlayer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("hello world")
+	animation_player.play("launch")
